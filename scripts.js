2f633cca7cbd43d4a1d96430c7ade39e9f85720b
Abel Tennison-Riley <attennisonriley@gmail.com>
2023-06-29 21:16:42 +0100

    add javascript to validate passwords

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function submitForm() {
+    var form = document.getElementById("signupForm");
+    form.submit();
+  }
+
+const passwordInput = document.getElementById('password');
+const passwordConfirmationInput = document.getElementById('passwordConfirmation');
+const passwordError = document.getElementById('passwordError');
+
+function validatePasswords(){
+    const password = passwordInput.ariaValueMax;
+    const passwordConfirmation = passwordConfirmationInput.value;
+
+    if (password !== passwordConfirmation){
+        passwordError.textContent = 'Passwords do not match';
+        passwordInput.classList.add('error');
+        passwordConfirmationInput.classList.add('error');
+    } else{
+        passwordError.textContent ='';
+        passwordInput.classList.remove('error');
+        passwordConfirmationInput.classList.remove('error');
+    }
+    }
+
+passwordConfirmationInput.addEventListener('input', validatePasswords);
